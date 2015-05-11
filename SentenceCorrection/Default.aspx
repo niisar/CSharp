e15842962c55a879dc6fe53fd85457e9f5163a0e
Niisar <nisar2048@gmail.com>
2015-05-11 13:17:34 +0530

    initial comm

---
+++ SentenceCorrection/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:TextBox TextMode="MultiLine" ID="t1" runat="server"></asp:TextBox>
+        <br />
+    <asp:TextBox TextMode="MultiLine" ID="t2" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button Text="click" runat="server" ID="btn" OnClick="btn_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ SentenceCorrection/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Globalization;
+using System.Text.RegularExpressions;
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btn_Click(object sender, EventArgs e)
+    {
+        var sourcestring = t1.Text;
+        var lowerCase = sourcestring.ToLower();
+        var r = new Regex(@"(^[a-z])|\.\s+(.)", RegexOptions.ExplicitCapture);
+        int x =Regex.Matches(sourcestring, @"(^[a-z])|\.\s+(.)").Count;
+        var result = r.Replace(lowerCase, s => s.Value.ToUpper());
+        t2.Text = result;
+        btn.Text = x.ToString();
+    }
+}
